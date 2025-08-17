5ec98ea62515a2d2b40de8dd734cd4cb7c390a83
Samip Lamsal <lamsalsamip@outlook.com>
2025-08-17 20:50:58 +0545

    add resource fetching

--- mcp_client.py
+++ mcp_client.py
@@ -1,6 +1,8 @@
 import sys
 import asyncio
 from typing import Optional, Any
+import json
+from pydantic import AnyUrl
 from contextlib import AsyncExitStack
 from mcp import ClientSession, StdioServerParameters, types
 from mcp.client.stdio import stdio_client
@@ -42,14 +44,13 @@ class MCPClient:
         return self._session
 
     async def list_tools(self) -> list[types.Tool]:
-        # TODO: Return a list of tools defined by the MCP server
-        return []
+        result = await self.session().list_tools()
+        return result.tools
 
     async def call_tool(
         self, tool_name: str, tool_input: dict
     ) -> types.CallToolResult | None:
-        # TODO: Call a particular tool and return the result
-        return None
+        return await self.session().call_tool(tool_name, tool_input)
 
     async def list_prompts(self) -> list[types.Prompt]:
         # TODO: Return a list of prompts defined by the MCP server
@@ -60,8 +61,12 @@ class MCPClient:
         return []
 
     async def read_resource(self, uri: str) -> Any:
-        # TODO: Read a resource, parse the contents and return it
-        return []
+        result = await self.session().read_resource(AnyUrl(uri))
+        resource = result.contents[0]
+        if isinstance(resource, types.TextResourceContents):
+            if resource.mime_type == "application/json":
+                return json.loads(resource.data)
+            return resource.data
 
     async def cleanup(self):
         await self._exit_stack.aclose()
@@ -82,7 +87,8 @@ async def main():
         command="uv",
         args=["run", "mcp_server.py"],
     ) as _client:
-        pass
+        result = await _client.list_tools()
+        print(result)
 
 
 if __name__ == "__main__":
--- mcp_server.py
+++ mcp_server.py
@@ -34,8 +34,23 @@ def edit_document(doc_id: str = Field(description="The ID of the document to edi
     docs[doc_id] = docs[doc_id].replace(old_string, new_string)
     return docs[doc_id]
 
-# TODO: Write a resource to return all doc id's
-# TODO: Write a resource to return the contents of a particular doc
+@mcp.resource(
+    "docs://documents",
+    mime_type="application/json",
+)
+def list_docs()->list[str]:
+    return list(docs.keys())
+
+@mcp.resource(
+    "docs://documents/{doc_id}",
+    mime_type="text/plain"
+)
+def fetch_doc(doc_id:str) -> str:
+    if doc_id not in docs:
+        raise ValueError(f"Document with ID '{doc_id}' not found.")
+    return docs[doc_id]
+
+
 # TODO: Write a prompt to rewrite a doc in markdown format
 # TODO: Write a prompt to summarize a doc
 
